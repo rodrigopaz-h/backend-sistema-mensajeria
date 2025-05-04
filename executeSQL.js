c97b87441635dfcf261a38206844c00604083d78
Rodrigo <rodrigo.pazhernandez@gmail.com>
2025-05-04 00:47:48 -0400

    Add SSL configuration to database client for secure connections

--- executeSQL.js
+++ executeSQL.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 async function executeSQLFromFile() {
