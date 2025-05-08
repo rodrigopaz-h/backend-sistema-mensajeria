470220376a081479368f5c8d2389a8dc0857924b
Rodrigo <rodrigo.pazhernandez@gmail.com>
2025-05-08 01:10:03 -0400

    Add file upload support to message sending route

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer(); // Archivos en memoria
 const { getMessages, sendMessage } = require("../controllers/messageController");
 const verifyToken = require("../middlewares/authMiddleware");
 
+// Obtener mensajes
 router.get("/", verifyToken, getMessages);
-router.post("/", verifyToken, sendMessage);
+
+// Enviar mensaje con posible archivo adjunto
+router.post("/", verifyToken, upload.single("file"), sendMessage);
 
 module.exports = router;
